3c9ac6ac158b313ccd76942183a347b950e0b514
RegisXV <139807025+RegisXV@users.noreply.github.com>
2024-02-14 11:25:33 -0500

    Database connection feel free to test!

---
+++ Website/createdb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    auth_plugin='mysql_native_password'
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE test")
+
+mycursor.execute("SHOW DATABASES")
+
+for db in mycursor:
+    print(db)
+    
