46afbe7e5ef44706093ab6a8110dc3f648075ad2
au4ob10 <agranchelli27@gmail.com>
2024-02-21 11:06:02 -0500

    Added account modal, text and buttons need to be aligned

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 # from Website import createdb
 
 app = Flask(__name__)
@@ -7,6 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+# @app.route('/button_press')
+# def button_press():
+#     return redirect(url_for('registration'))
+
+# @app.route('/registration')
+# def registration():
+#     return render_template('reg.html')
+
+
+
+
 if __name__=='__main__':
     app.run(debug=True)
+
+
+# @app.route('/register', methods=["POST", "GET"])
+# def regPage():
+#     return render_template("reg.html")
+
+
     
