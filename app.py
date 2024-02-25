3b9e49acb6fcb07181f01e37bcd06e89d3be49db
au4ob10 <agranchelli27@gmail.com>
2024-02-24 19:17:11 -0500

    changed mysql table structure

--- app.py
+++ app.py
@@ -8,24 +8,7 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/button_press')
-# def button_press():
-#     return redirect(url_for('registration'))
-
-# @app.route('/registration')
-# def registration():
-#     return render_template('reg.html')
-
-
-
-
 if __name__=='__main__':
     app.run(debug=True)
-
-
-# @app.route('/register', methods=["POST", "GET"])
-# def regPage():
-#     return render_template("reg.html")
-
 
     
