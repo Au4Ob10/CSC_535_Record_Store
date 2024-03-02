f588ef9d72e07ea269a5d03858d053ef2dca6ec4
au4ob10 <agranchelli27@gmail.com>
2024-03-02 13:27:26 -0500

    created static folder for images. Populated homepage

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 # from Website import createdb
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
