c32c3639c6a3b1571e011a66034f971dcd78d587
au4ob10 <agranchelli27@gmail.com>
2024-03-06 16:44:43 -0500

    reverted back

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,33 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import mysql.connector
+import pymysql
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
-from os import environ
+import os
+
+
 
 app = Flask(__name__)
 app.secret_key = "csc-535-record-store-app"
 
-load_dotenv('.flaskenv')
-app.config['MYSQL_HOST'] = environ.get("MYSQL_IP")
-app.config['MYSQL_USER'] = environ.get("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = environ.get("MYSQL_PASS")
-app.config['MYSQL_DB'] = environ.get("MYSQL_DB")
-app.config['MYSQL_PORT'] = environ.get('MYSQL_PORT')
-
-mysql = MySQL(app)
 
 
 
 
+
+
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_IP")
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASS")
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+
+mysql = mydb.cursor()
 def init_cursor():
-    return mysql.connection.cursor(cursors.DictCursor)
+return mysql._connection.cursor(dictionary=True)
 
 
-
-
-with app.app_context():
-    print("App is running")
-
-    from app.routes import *
-    from app.database import *
 
 #read_sql("create_tables")
