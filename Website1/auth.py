67fe7228dbecfeccd91cfc5fad5ccd5ff39d4086
Diego Aguilar <daaguilar21@gmail.com>
2024-03-12 20:24:00 -0400

    initialize_database() function in app.py

--- Website1/auth.py
+++ Website1/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, session, redirect, render_template, url_for, flash
-from app import store_db, cur
+from app import cur
 auth1 = Blueprint('auth1',__name__,static_folder="Website1/static", template_folder='Website1/templates')
 
 
