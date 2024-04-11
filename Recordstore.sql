04b3cc48551dfc7ea95b295e734c74b7e51ebc55
RegisXV <139807025+RegisXV@users.noreply.github.com>
2024-04-11 11:01:07 -0400

    database fix and when you create an account you will be logged in

--- Recordstore.sql
+++ Recordstore.sql
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `record_store`.`customer` (
   `email` VARCHAR(45) NULL,
   `passw` VARCHAR(45) NULL,
   `phone_num` varchar(20) NOT NULL,
-  `cart#` INT Default 0,
+  `cart` INT Default 0,
   `if_register` TINYINT(1) NULL,
   PRIMARY KEY (`customer_id`)
 ) ENGINE = InnoDB;
